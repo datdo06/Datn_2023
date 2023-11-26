804bb3114e415db54e8d92b69f07a80d04b0b420
Do tien dat <datdtph25033@fpt.edu.vn>
2023-11-26 20:59:04 +0700

    update Blade mail message reset password

--- resources/views/mail/forget-password.blade.php
+++ resources/views/mail/forget-password.blade.php
@@ -1,1 +1,2 @@
-<a href="{{ route('reset.password',  $token) }}">reset password</a>
+<h2>Bấm vào đường dẫn dưới để đặt lại mật khẩu</h2>
+<a href="{{ route('reset.password',  $token) }}">Đường dẫn</a>
